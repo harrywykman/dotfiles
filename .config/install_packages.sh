9120caa7dfa3c12d935c5664ba6ef7be907d2d75
archaeon@goldman <goldman+hello@harrywykman.com>
2019-07-21 20:10:42 +0800

    added package lists and install script

---
+++ .config/install_packages.sh
@@ -0,0 +1,2 @@
+sudo apt-mark auto $(cat packages_auto.list)
+sudo apt-mark manual $(cat packages_manual.list)
---
+++ .config/update_packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-mark showauto > packages_auto.list
+apt-mark showmanual > packages_manual.list
